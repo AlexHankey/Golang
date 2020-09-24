366d53a4883d44efb299b68fb1d79819c846c8b1
Alex <alex.hankey@myclevergroup.com>
2020-09-24 14:00:11 +0100

    Simple integer testing

---
+++ integers/adder_test.go
@@ -0,0 +1,18 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T)  {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+	if sum == expected {
+		fmt.Println("This is the passed result:", sum)
+	}
+}
