d3524ae55016287d9a99c04833bd1dafe8e9855e
Alex <alex.hankey@myclevergroup.com>
2020-09-24 09:19:31 +0100

    Refectored hello world test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,11 +2,22 @@ package main
 
 import "testing"
 
-func TestHello (t *testing.T) {
-	got := Hello()
-	want := "Hello, Worlds"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+func TestHello(t *testing.T)  {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	//t.Run("saying hello to people", func(t *testing.T) {
+	//	got := Hello("Alex")
+	//	want := "Hello, Alex"
+	//	assertCorrectMessage(t, got, want)
+	//})
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
