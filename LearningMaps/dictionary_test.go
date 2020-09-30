342ff1c21b138938ef3a45da821fd56a3633fe97
Alex <alex.hankey@myclevergroup.com>
2020-09-30 10:59:49 +0100

    Writing Test to add a word to a dictionary and check wether that word has got a definition or not

--- LearningMaps/dictionary_test.go
+++ LearningMaps/dictionary_test.go
@@ -3,12 +3,36 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is the meaning of test"
+
+	dictionary.Add(word, definition)
+	assertDefinition(t, dictionary, word, definition)
+}
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word", err)
+	}
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
 	}
 }
