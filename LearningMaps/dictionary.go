7bea3b08f64c4a0d69a5a57565ae011471d463bc
Alex <alex.hankey@myclevergroup.com>
2020-09-30 15:14:57 +0100

    worked on map tutorial + learned about Dependency Injection with tests

--- LearningMaps/dictionary.go
+++ LearningMaps/dictionary.go
@@ -2,10 +2,20 @@ package main
 
 type Dictionary map[string]string
 
+func (d Dictionary) Error() string {
+	panic("implement me")
+}
+
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
+
+func (d Dictionary) Update(word, definition string) error {
+	d[word] = definition
+	return nil
+}
 
 type DictionaryErr string
 
@@ -33,8 +43,4 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
-}
-
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
 }
