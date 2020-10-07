5c56ae1d5a9f74879bff961af049d0faf3ef6e96
Alex <alex.hankey@myclevergroup.com>
2020-10-07 11:57:03 +0100

    Created a simple quiz

---
+++ Pointers/practicalProgramming/point.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//point to Memory address &
+// Read through Memory address *
+
+func main() {
+	x := 15
+	a := &x
+	fmt.Println(a)
+	fmt.Println(*a)
+
+	*a = 5
+	fmt.Println(x)
+	*a = *a * *a // 5 * 5 = 25
+	fmt.Println(x)
+	fmt.Println(*a)
+
+}
